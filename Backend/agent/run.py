12b9bf1f42dfabaed6476ad55559541f2254a92a
DLab1402 <phankhanhduy.tg.vl@gmail.com>
2025-08-07 18:46:46 +0700

    7/8/2025

--- Backend/agent/run.py
+++ Backend/agent/run.py
@@ -1,11 +1,17 @@
 from ai_agent import create_agent
 import uvicorn
+import shutil
 import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from utils.global_backend import BACKEND_IP,BACKTEND_PORT
+from utils.global_backend import BACKEND_IP,BACKTEND_PORT, UPLOAD_DIR
+
+for item in os.listdir(UPLOAD_DIR):
+    item_path = os.path.join(UPLOAD_DIR, item)
+    if os.path.isdir(item_path):
+        shutil.rmtree(item_path)
 
 agent = create_agent()
 
